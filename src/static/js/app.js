e974979a5176950117183891d68242115e42d0c4
souhaib-hsoumi <60830330+souhaib-hsoumi@users.noreply.github.com>
2022-08-01 19:47:25 +0100

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -92,7 +92,7 @@ function AddItemForm({ onNewItem }) {
     return (
 
         <Form onSubmit={submitNewItem}>
-                    <h1>Application for Testing</h1>
+                    <h1>Application for testing</h1>
                     
 
 
